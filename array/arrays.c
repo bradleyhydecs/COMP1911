de46a2fede72639f4be1d481c8c98211ae2b292f
Bradley Hyde <bradleyhydecs@users.noreply.github.com>
2017-03-22 00:12:04 +1100

    Separated into folders

---
+++ array/arrays.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char* argv[]) {
+
+    int numbers[10] = {9,8,7,6,5,4,3,2,1,55};
+
+    printf("element at index 0 = %d \n", numbers[0]);
+    printf("element at index 1 = %d \n", numbers[1]);
+
+    printf("element at index 9 = %d \n", numbers[9]);
+    printf("element at index 10 = %d \n", numbers[10]);
+
+    
+    return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ while/diamond.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char* argv[]) 
+{
+    // read our number in
+    int length;
+    printf("Enter side length: ");
+    scanf("%d", &length);
+
+    // loop over rows (lines)
+    int row = 0;
+    while (row < (length * 2) - 1) 
+    {
+        // loop over cols (each character)
+        int col = 0;
+        while (col < (length * 2) - 1) 
+        {
+            // if this is the top half of the diamond
+            if (row < length)
+            {
+                //    *                           *       
+                //   *                             *        
+                //  *                               *      
+                // *                                 *     
+                if (col == (length-1) - row || col == (length-1) + row)
+                {
+                    printf("*");                   
+                }        
+                else
+                {
+                    printf("_");                   
+                }
+            }
+            // else we are up to the bottom half
+            else
+            {
+                // *                                 *       
+                //  *                               *        
+                //   *                             *      
+                //    *                           * 
+                if (col == row - (length - 1) || col == (2*(length-1)) - (row-(length-1)) )
+                {
+                    printf("*");                   
+                }        
+                else
+                {
+                    printf("_");                   
+                }
+            }
+            col = col + 1;
+        }
+        printf("\n");                   
+        row = row + 1;
+    } 
+    
+    return EXIT_SUCCESS;
+}
